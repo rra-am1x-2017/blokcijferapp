ec8d064183bbcf00e1df402ac2d45777c5160722
Arjan de Ruijter <rra.am1a.2017@gmail.com>
2018-04-18 06:59:44 +0200

    Dit is een test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public todos = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+  }
 
+  public openTodoAlert() {
+    let addTodoAlert = this.alertCtrl.create({
+      title: "Klusje",
+      message: "Tik een nieuwe klus in",
+      inputs: [
+        {
+          type: "text",
+          name: "nameTodoInput"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel"
+        },
+        {
+          text: "Voeg toe",
+          handler: (inputData) => {
+            let todoText;
+            todoText = inputData.nameTodoInput;
+            this.todos.push(todoText);
+          }
+        }
+
+      ]
+    });
+    addTodoAlert.present();
   }
 
 }
